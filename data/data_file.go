27fe3fcf6df46aed50ddb238d063d03fa107140b
Morning <springoflotus@gmail.com>
2025-11-28 20:48:06 +0800

    feat：初始化 OpenDataFile 方法实现

--- data/data_file.go
+++ data/data_file.go
@@ -8,3 +8,8 @@ type DataFile struct {
 	WriteOff  int64         // 文件写到了哪个位置
 	IoManager fio.IOManager // io 读写管理
 }
+
+// OpenDataFile 打开新的数据文件
+func OpenDataFile(dirPath string, fileId uint32) (*DataFile, error) {
+	return nil, nil
+}
