e09da9ecc13588d2f3b25eb0312fa9e63d4fcac5
Morning <springoflotus@gmail.com>
2025-11-28 19:50:48 +0800

    feat：创建 FileIO 初始化函数

--- fio/file_io.go
+++ fio/file_io.go
@@ -7,6 +7,19 @@ type FileIO struct {
 	fd *os.File // 系统文件描述符
 }
 
+// NewFileIOManager 初始化标准文件 IO
+func NewFileIOManager(fileName string) (*FileIO, error) {
+	fd, err := os.OpenFile(
+		fileName,
+		os.O_CREATE|os.O_RDWR|os.O_APPEND,
+		DataFilePerm,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &FileIO{fd: fd}, nil
+}
+
 // Read 从文件的给定位置读取对应的数据
 func (fio *FileIO) Read(b []byte, offset int64) (int, error) {
 	return fio.fd.ReadAt(b, offset)
