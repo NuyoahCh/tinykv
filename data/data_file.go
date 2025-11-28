1aabdcfa7fd69428f6a1f1f77be8da531e4b4c8b
Morning <springoflotus@gmail.com>
2025-11-28 20:39:18 +0800

    feat：初始化 DataFile 数据文件字段

---
+++ data/data_file.go
@@ -0,0 +1,10 @@
+package data
+
+import "github.com/Nuyoahch/tinykv/fio"
+
+// DataFile 数据文件
+type DataFile struct {
+	FileId    uint32        // 文件id
+	WriteOff  int64         // 文件写到了哪个位置
+	IoManager fio.IOManager // io 读写管理
+}
