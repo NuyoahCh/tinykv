48cfbd4b27a2d89332cbe5a6cda09d61fff172b8
Morning <springoflotus@gmail.com>
2025-11-28 21:56:58 +0800

    opt：完善 error 模块类型参数

--- error.go
+++ error.go
@@ -4,8 +4,9 @@ import "errors"
 
 // 错误类型参数
 var (
-	ErrKeyIsEmpty        = errors.New("the key is empty")
-	ErrIndexUpdateFailed = errors.New("failed to update index")
-	ErrKeyNotFound       = errors.New("key not found in database")
-	ErrDataFileNotFound  = errors.New("data file is not found")
+	ErrKeyIsEmpty             = errors.New("the key is empty")
+	ErrIndexUpdateFailed      = errors.New("failed to update index")
+	ErrKeyNotFound            = errors.New("key not found in database")
+	ErrDataFileNotFound       = errors.New("data file is not found")
+	ErrDataDirectoryCorrupted = errors.New("the database directory maybe corrupted")
 )
