1242737b53bd1d00a9bd3c783118de2c60792677
Morning <springoflotus@gmail.com>
2025-11-28 20:32:58 +0800

    feat：初始化 LogRecord 写入数据文件记录

--- data/log_record.go
+++ data/log_record.go
@@ -1,5 +1,22 @@
 package data
 
+type LogRecordType = byte
+
+// 初始化相关类型参数
+const (
+	// LogRecordNormal 正常的类型
+	LogRecordNormal LogRecordType = iota
+	// LogRecordDeleted 针对数据删除的类型
+	LogRecordDeleted
+)
+
+// LogRecord 写入到数据文件的记录
+type LogRecord struct {
+	Key   []byte
+	Value []byte
+	Type  LogRecordType
+}
+
 // LogRecordPos 数据内存索引，描述数据在磁盘位置
 type LogRecordPos struct {
 	Fid    uint32 // 文件 id，表示数据存储的文件标识
