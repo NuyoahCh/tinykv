62d9dc9647a138da9f231b4eaa66e013ac3dce69
Morning <springoflotus@gmail.com>
2025-11-29 23:52:54 +0800

    opt：完善 IO 操作类型参数

--- fio/io_manager.go
+++ fio/io_manager.go
@@ -3,6 +3,15 @@ package fio
 // DataFilePerm 数据文件访问常量
 const DataFilePerm = 0644
 
+// FileIOType 文件 IO 类型
+type FileIOType = byte
+
+// IO 操作类型参数
+const (
+	StandardFile FileIOType = iota
+	MemoryMap
+)
+
 // IOManager 抽象 IO 管理接口，可接入不同的 IO 类型
 type IOManager interface {
 	// Read 从文件的给定位置读取对应的数据
@@ -21,7 +30,14 @@ type IOManager interface {
 	Size() (int64, error)
 }
 
-// NewIOManager 初始化 IOManager，目前只支持标准的 FileIO
-func NewIOManager(fileName string) (IOManager, error) {
-	return NewFileIOManager(fileName)
+// NewIOManager 初始化 IOManager，目前只支持标准 FileIO
+func NewIOManager(fileName string, ioType FileIOType) (IOManager, error) {
+	switch ioType {
+	case StandardFile:
+		return NewFileIOManager(fileName)
+	case MemoryMap:
+		return NewMMapIOManager(fileName)
+	default:
+		panic("unsupported io type")
+	}
 }
