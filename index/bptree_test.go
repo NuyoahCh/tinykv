70ef4d659fd10c543be91d58e21d4ea1952cb508
Morning <springoflotus@gmail.com>
2025-11-29 16:28:06 +0800

    opt：完善 B+ 树测试方法

--- index/bptree_test.go
+++ index/bptree_test.go
@@ -1,1 +1,27 @@
 package index
+
+import (
+	"go.etcd.io/bbolt"
+	"testing"
+)
+
+func TestBPlusTree_Iterator(t *testing.T) {
+	db, _ := bbolt.Open("/tmp/bbolt", 0644, nil)
+	db.Update(func(tx *bbolt.Tx) error {
+		bucket, _ := tx.CreateBucketIfNotExists([]byte("name"))
+		bucket.Put([]byte("bbccde"), []byte("b1"))
+		bucket.Put([]byte("cchune"), []byte("b1"))
+		bucket.Put([]byte("bbcaed"), []byte("b1"))
+		bucket.Put([]byte("aacded"), []byte("b1"))
+		bucket.Put([]byte("ccdeas"), []byte("b1"))
+		return nil
+	})
+
+	db.View(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte("name"))
+		cursor := bucket.Cursor()
+		k, _ := cursor.Seek([]byte("bb"))
+		t.Log(string(k))
+		return nil
+	})
+}
