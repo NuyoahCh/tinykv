660505da028d8bc9dbd830681112e397aee68efe
Morning <springoflotus@gmail.com>
2025-11-29 15:06:35 +0800

    opt：修改项目测试

--- merge_test.go
+++ merge_test.go
@@ -1,6 +1,7 @@
 package tinykv
 
 import (
+	"github.com/Nuyoahch/tinykv/utils"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -15,6 +16,13 @@ func TestDB_Merge(t *testing.T) {
 	//defer destroyDB(db)
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
+
+	//keys := db.ListKeys()
+	//t.Log(len(keys))
+
+	val, err := db.Get(utils.GetTestKey(99033))
+	t.Log(string(val))
+	t.Log(err)
 
 	//for i := 0; i < 500000; i++ {
 	//	db.Put(utils.GetTestKey(i), utils.RandomValue(128))
